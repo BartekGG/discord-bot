08f19090a7ca543ec1832da742279a9edbf3dd1a
Bartosz <43760209+BartekGG@users.noreply.github.com>
2022-02-10 23:54:41 +0000

    Hello World

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("kuziem kot")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f"We have logged in as {client}")
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("Hello"):
+    await message.channel.send("World!")
+
+my_secret = os.environ['TOKEN']
+client.run(my_secret)
